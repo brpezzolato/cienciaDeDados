f02ab667e49bc2e3b5f4399ee0441e097085b763
brpezzolato <bruno.pezzolato@icloud.com>
2026-02-12 00:47:37 -0300

    Todas as att de loop

--- lacos/atividade5.py
+++ lacos/atividade5.py
@@ -1,6 +1,6 @@
 num = int(input("Digite o numero que voce quer saber a tabuada: "))
 
-                  # Valor inicial, Valor final, Incremento ou Decremento
+# Valor inicial, Valor final, Incremento ou Decremento
 for count in range(1, 11, 1):
     resul = count * num
     print(f"{num} x {count} = {resul}")
---
+++ lacos/atividade6.py
@@ -0,0 +1,5 @@
+for i in range(1, 11, 1):
+    print(f'\nTabuada do {i}: ')
+    for count in range(1, 11, 1):
+        resul = count * i
+        print(f"{i} x {count} = {resul}")
